255e653e5d5226f454a89feca38418b3f8e53a28
Abhidha Dixit <abhidhadixit0430@gmail.com>
2024-08-29 23:38:37 +0530

    commit changes done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
-import Hook from './components/Hook';  
-import Hookcounter2 from './components/Hookcounter2';
-import Hookcounter3 from './components/Hookcounter3';
-
+// import Hook from './components/Hook';  
+// import Hookcounter2 from './components/Hookcounter2';
+// import Hookcounter3 from './components/Hookcounter3';
+// import Hookcounter4 from './components/Hookcounter4';
 function App() {
   return (
     <>
     <Hook />
     <Hookcounter2 />
     <Hookcounter3 />
+    < Hookcounter4 />
     </>
   );
 }
--- my-app/src/components/Hookcounter3.js
+++ my-app/src/components/Hookcounter3.js
@@ -3,8 +3,8 @@ function Hookcounter3(){
   const [name,setname]=useState({firstname:'',lastname:''})
   return (
     <form>
-        <input type='text' value={name.firstname} onChange={e=>setname({firstname:e.target.value})}></input>
-        <input type='text' value={name.lastname} onChange={e=>setname({lastname:e.target.value})}></input>
+        <input type='text' value={name.firstname} onChange={e=>setname({...name,firstname:e.target.value})}></input>
+        <input type='text' value={name.lastname} onChange={e=>setname({...name,lastname:e.target.value})}></input>
          
          <h2> Your First Name-{name.firstname}</h2>
          <h2>Yout Last Name-{name.lastname}</h2>
