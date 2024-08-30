a84f44dc53e0e8abd96ba31e35188bd73bf160f2
Abhidha Dixit <abhidhadixit0430@gmail.com>
2024-08-30 20:29:48 +0530

    done

---
+++ my-app/src/components/Datafetching.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+
+function Datafetching() {
+    useState()
+  return (
+    
+    <div>Datafetching</div>
+  )
+}
+
+export default Datafetching
