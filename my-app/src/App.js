8377e7ded84a4927ccc55b5c34fae8b0bcf2edd1
Abhidha Dixit <abhidhadixit0430@gmail.com>
2024-08-31 19:25:44 +0530

    commit changes done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,23 @@
- import Hooktypeone from './components/Hooktypeone';  
-import Hooktypetwo from './components/Hooktypetwo';
-import Hooktypethree from './components/Hooktypethree';
-import Hooktypefour from './components/Hooktypefour';
-// import UseEffect1 from './components/UseEffectone';
-import Mousehook from './components/Mousehook';
-import UseEffectone from './components/UseEffecttwo';
-import UseEffecttwo from './components/UseEffecttwo';
+//  import Hooktypeone from './components/Hooktypeone';  
+// import Hooktypetwo from './components/Hooktypetwo';
+// import Hooktypethree from './components/Hooktypethree';
+// import Hooktypefour from './components/Hooktypefour';
+// import Mousehook from './components/Mousehook';
+// import UseEffectone from './components/UseEffecttwo';
+// import UseEffecttwo from './components/UseEffecttwo';
+import Datafetching from './components/Datafetching';
+
 function App() {
   return (
     <>
-    <Hooktypeone />
+    {/* <Hooktypeone />
     <Hooktypetwo />
     <Hooktypethree />
     < Hooktypefour />
     <UseEffectone />
-    <UseEffecttwo />
-    <Mousehook />
+    <UseEffecttwo /> */}
+    {/* <Mousehook /> */}
+    <Datafetching/>
     </>
   );
 }
--- my-app/src/components/Datafetching.js
+++ my-app/src/components/Datafetching.js
@@ -1,12 +1,36 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Datafetching() {
-    useState()
-  return (
-    
-    <div>Datafetching</div>
-  )
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const clickHandle = () => {
+        setIdFromButtonClick(id)
+    }
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
+            .then(res => {
+                console.log(res)
+                setPost(res.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [idFromButtonClick])
+
+    return (
+        <div>
+            <input type='text' value={id} onChange={e => setId(e.target.value)} />
+            <button type='button' onClick={clickHandle}>Fetch Post</button>
+            <div>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Datafetching
