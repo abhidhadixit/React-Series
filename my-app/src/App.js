4ac1e8a666ce39b366032b1c3f64d72e40aacdbf
Abhidha Dixit <abhidhadixit0430@gmail.com>
2024-09-02 19:41:39 +0530

    changes done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,41 @@
-import React from 'react';
-import ComponentC from './components/ComponentC';
-//  import Hooktypeone from './components/Hooktypeone';  
-// import Hooktypetwo from './components/Hooktypetwo';
-// import Hooktypethree from './components/Hooktypethree';
-// import Hooktypefour from './components/Hooktypefour';
-// import Mousehook from './components/Mousehook';
-// import UseEffectone from './components/UseEffecttwo';
-// import UseEffecttwo from './components/UseEffecttwo';
-// import Datafetching from './components/Datafetching';
+// // import React from 'react';
+// // import ComponentC from './components/ComponentC';
+// // import Hooktypeone from './components/Hooktypeone';  
+// // import Hooktypetwo from './components/Hooktypetwo';
+// // import Hooktypethree from './components/Hooktypethree';
+// // import Hooktypefour from './components/Hooktypefour';
+// // import Mousehook from './components/Mousehook';
+// // import UseEffectone from './components/UseEffecttwo';
+// // import UseEffecttwo from './components/UseEffecttwo';
+// // import Datafetching from './components/Datafetching';
 
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+// import React from 'react';
+// import Rcounterone from './components/Rcounterone';
 
-function App() {
-  return (
-    <>
-     {/* <Hooktypeone />
-    <Hooktypetwo />
-    <Hooktypethree />
-    < Hooktypefour />
-    <UseEffectone />
-    <UseEffecttwo /> */}
-    {/* <Mousehook /> */}
-    {/* <Datafetching/> */}
-    
-    <UserContext.Provider value={'sona'}>
-      <ChannelContext.Provider value={'YouTube'}>
-        <ComponentC />
-      </ChannelContext.Provider>
-    </UserContext.Provider>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <Rcounterone />
+//       {/* Uncomment the following lines if needed */}
+//       {/* <Hooktypeone /> */}
+//       {/* <Hooktypetwo /> */}
+//       {/* <Hooktypethree /> */}
+//       {/* <Hooktypefour /> */}
+//       {/* <UseEffectone /> */}
+//       {/* <UseEffecttwo /> */}
+//       {/* <Mousehook /> */}
+//       {/* <Datafetching/> */}
+      
+//       {/* <UserContext.Provider value={'sona'}>
+//         <ChannelContext.Provider value={'YouTube'}>
+//           <ComponentC />
+//         </ChannelContext.Provider>
+//       </UserContext.Provider> */}
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ my-app/src/App2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counterone from './components/Counterone.jsx'; 
+
+function App2() {
+  return (
+    <div className='App'>
+      {/* <h1>Counter App</h1> */}
+      <Counterone />
+    </div>
+  );
+}
+
+export default App2;
