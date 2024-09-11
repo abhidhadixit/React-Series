89600fda36dbdd84e79b38c247ac16243d13e9a2
Abhidha Dixit <abhidhadixit0430@gmail.com>
2024-09-11 19:53:10 +0530

    changes done

--- my-tailwind-project/Currency-Converter/src/App.jsx
+++ my-tailwind-project/Currency-Converter/src/App.jsx
@@ -1,16 +1,17 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Input} from '.c/omponents'
+import CurrencyConverterinfo from './Hooks/CurrencyConverterinfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const [amount,setAmount]=useState(0)
+    const [from ,setFrom]=useState("usd")
+    const [to ,setTo]=useState("inr")
+    const [convertedAmount,setConvertedAmount]=useState(0)
   return (
-    <>
-    <h1>hey good morning </h1>
-    </>
-  )
+    <div>App</div>
+  ) 
 }
 
 export default App
